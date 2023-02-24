0ca6a5a3f5a735c9afac124d76b66d678d29cbf4
TazkieCT <tazkiect25@gmail.com>
2023-02-24 22:42:18 +0700

    Delete useless

--- Belajar/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-
-@section('container')
-    <div class="d-flex justify-content-center mb-3">
-        <img src="{{ asset('img/SMK.jpg') }}" alt="Sekolah" width="700">
-    </div>
-    <p>Telkom Schools merupakan perubahan dari Sekolah Telkom Sandhyputra yang dikelola oleh Yayasan Sandhykara Putra Telkom (YSPT). Peluncuran Telkom Schools dilaksanakan di SMK Telkom Jakarta pada tanggal 23 Januari 2014 oleh Dirjen Pendidikan Menengah Kemendikbud RI Prof. Dr. Ir. Achmad Jazidie beserta Dewan Pengurus dan Pembina Yayasan Sandhykara Putra Telkom.</p>
-    <a href="/form" class="btn btn-danger">Formulir Aspirasi</a>
-@endsection
